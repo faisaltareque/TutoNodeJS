a5134b7c88a972146d017e611d3913f9eff735fc
Faisal Tareque Shohan <faishaltareque@gmail.com>
2020-07-03 14:16:58 +0600

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,10 @@
 //const add = require('./utils.js')
-const fs = require('fs')
+//const fs = require('fs')
+const validator = require('validator')
 const getNotes =  require('./notes.js')
 
 const msg=getNotes()
-
-console.log(msg)
 // console.log(add(5,8))
+//console.log(msg)
+console.log(validator.isURL('https://www.google.com'))
+
