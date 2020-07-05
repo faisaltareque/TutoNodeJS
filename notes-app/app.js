5ebfa81e49588aea919963a6f3e3ba9cc471b21c
Faisal Tareque Shohan <faishaltareque@gmail.com>
2020-07-05 19:19:56 +0600

    Updated notes-app/app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,6 +4,7 @@ const validator = require('validator')
 const getNotes =  require('./notes.js')
 
 const msg=getNotes()
+
 // console.log(add(5,8))
 //console.log(msg)
 console.log(validator.isURL('https://www.google.com'))
