ba0ab1aa9cc07cc587eb4037afcf865cb4c5e028
Faisal Tareque Shohan <faishaltareque@gmail.com>
2020-07-18 01:23:00 +0600

    Update

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,16 +2,24 @@
 // const fs = require('fs')
 // const validator = require('validator')
  const getNotes =  require('./notes.js')
+ const yargs = require('yargs')
  const chalk = require('chalk')
 
 
-const msg=getNotes()
-console.log(msg)
-console.log(chalk.red.bgGreen.bold('Success'));
+//const msg=getNotes()
+//console.log(msg)
+//console.log(chalk.red.bgGreen.bold('Success'));
 // console.log(add(5,8))
 // console.log(validator.isURL('https://www.google.com'))
 
-console.log(process.argv)
+
+// Customizing Yargs Version
+yargs.version('1.1.0')
+
+const command=process.argv
+console.log(command)
+console.log(yargs.argv)
+
 
  
 
