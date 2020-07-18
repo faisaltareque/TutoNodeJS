225ee78fe5ab8acaf7b5b57f075ec838f4c129ae
Faisal Tareque Shohan <faishaltareque@gmail.com>
2020-07-19 03:27:06 +0600

    Update

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,20 +5,66 @@
  const yargs = require('yargs')
  const chalk = require('chalk')
 
-
 //const msg=getNotes()
 //console.log(msg)
 //console.log(chalk.red.bgGreen.bold('Success'));
 // console.log(add(5,8))
 // console.log(validator.isURL('https://www.google.com'))
 
-
 // Customizing Yargs Version
 yargs.version('1.1.0')
 
-const command=process.argv
-console.log(command)
-console.log(yargs.argv)
+// add, remove, read, list
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type : 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type : 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('Title: '+ argv.title)
+        console.log('Body: '+argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a note',
+    handler: function(){
+        console.log(chalk.green.bgGray.bold('Removing the Note'))
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log(chalk.green.bgGray.bold('Reading the Note'))
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log(chalk.green.bgGray.bold('Listing out all your notes'))
+    }
+})
+
+yargs.parse()
 
 
  
